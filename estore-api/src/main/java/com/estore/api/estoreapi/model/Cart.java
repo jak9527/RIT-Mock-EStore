0f575437cd35029f7f797a4b78e46dac57c51b39
Jacob Karvelis <jak9527@rit.edu>
2023-03-03 12:32:37 -0500

    Forgot to add the Cart class to the git, so that last commit message probably didn't make much sense. Added it now. Refer to previous commit message

---
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Cart.java
@@ -0,0 +1,49 @@
+package com.estore.api.estoreapi.model;
+
+import java.util.logging.Logger;
+import java.util.HashMap;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+/**
+ * Represents a Product entity
+ * 
+ * @author SWEN Faculty
+ */
+public class Cart {
+    private static final Logger LOG = Logger.getLogger(Product.class.getName());
+
+    // Package private for tests
+    static final String STRING_FORMAT = "Cart [id=%d, products=%s]";
+
+    @JsonProperty("id") private int id;
+    @JsonProperty("products") private HashMap<Integer, Product> products; //map product id to product
+
+    /**
+     * Create a Cart with the given id
+     * @param id The id of the product
+     * t
+     * 
+     * {@literal @}JsonProperty is used in serialization and deserialization
+     * of the JSON object to the Java object in mapping the fields.  If a field
+     * is not provided in the JSON object, the Java field gets the default Java
+     * value, i.e. 0 for int
+     */
+    public Cart(@JsonProperty("id") int id) {
+        this.id = id;
+        this.products = new HashMap<>();
+    }
+
+    /**
+     * Retrieves the id of the product
+     * @return The id of the product
+     */
+    public int getId() {return id;}
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return String.format(STRING_FORMAT,id,products);
+    }
+}
