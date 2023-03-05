5ddf02acbbbb9e955c521bba54738bf231a2421e
Kelly Showers <kds1653@rit.edu>
2023-03-05 16:48:05 -0500

    basic User file test done

---
+++ estore-api/src/test/java/com/estore/api/estoreapi/persistence/UserFileDAOTest.java
@@ -0,0 +1,125 @@
+package com.estore.api.estoreapi.persistence;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.estore.api.estoreapi.model.User;
+import com.estore.api.estoreapi.persistence.UserFileDAO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test the Product File DAO class
+ * 
+ * @author SWEN Faculty
+ */
+@Tag("Persistence-tier")
+public class UserFileDAOTest {
+    UserFileDAO userFileDAO;
+    User[] testUser;
+    ObjectMapper mockObjectMapper;
+
+    /**
+     * Before each test, we will create and inject a Mock Object Mapper to
+     * isolate the tests from the underlying file
+     * @throws IOException
+     */
+    @BeforeEach
+    public void setupUserFileDAO() throws IOException {
+        mockObjectMapper = mock(ObjectMapper.class);
+        testUser = new User[3];
+        testUser[0] = new User(99,"Wi-Fire");
+        testUser[1] = new User(100,"Galactic Agent");
+        testUser[2] = new User(101,"Ice Gladiator");
+
+        // When the object mapper is supposed to read from the file
+        // the mock object mapper will return the product array above
+        when(mockObjectMapper
+            .readValue(new File("doesnt_matter.txt"),User[].class))
+                .thenReturn(testUser);
+        userFileDAO = new UserFileDAO("doesnt_matter.txt",mockObjectMapper);
+    }
+
+    @Test
+    public void testGetUser() {
+        // Invoke
+        User user = userFileDAO.getUser("Wi-Fire");
+
+        // Analzye
+        assertEquals(user,testUser[0]);
+    }
+
+
+    @Test
+    public void testCreateUser() {
+        // Setup
+        User user = new User(102,"Wonder-Person");
+
+        // Invoke
+        User result = assertDoesNotThrow(() -> userFileDAO.createUser(user),
+                                "Unexpected exception thrown");
+
+        // Analyze
+        assertNotNull(result);
+        User actual = userFileDAO.getUser(user.getUsername());
+        assertEquals(actual.getId(),user.getId());
+        assertEquals(actual.getUsername(),user.getUsername());
+    }
+
+
+    @Test
+    public void testSaveException() throws IOException{
+        doThrow(new IOException())
+            .when(mockObjectMapper)
+                .writeValue(any(File.class),any(User[].class));
+
+        User user = new User(102,"Fungi");
+
+        assertThrows(IOException.class,
+                        () -> userFileDAO.createUser(user),
+                        "IOException not thrown");
+    }
+
+    @Test
+    public void testGetUserNotFound() {
+        // Invoke
+        User user = userFileDAO.getUser("BigDude");
+
+        // Analyze
+        assertEquals(user,null);
+    }
+
+
+    @Test
+    public void testConstructorException() throws IOException {
+        // Setup
+        ObjectMapper mockObjectMapper = mock(ObjectMapper.class);
+        // We want to simulate with a Mock Object Mapper that an
+        // exception was raised during JSON object deseerialization
+        // into Java objects
+        // When the Mock Object Mapper readValue method is called
+        // from the ProductFileDAO load method, an IOException is
+        // raised
+        doThrow(new IOException())
+            .when(mockObjectMapper)
+                .readValue(new File("doesnt_matter.txt"),User[].class);
+
+        // Invoke & Analyze
+        assertThrows(IOException.class,
+                        () -> new UserFileDAO("doesnt_matter.txt",mockObjectMapper),
+                        "IOException not thrown");
+    }
+}
