ca15b15047fcd87b01a17f55b25d4364cccf79d1
epm2875 <epm2875@rit.edu>
2023-03-09 19:05:14 -0500

    Updated code comments

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/UserController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/UserController.java
@@ -31,7 +31,7 @@ public class UserController {
         /**
      * Creates a REST API controller to reponds to requests
      * 
-     * @param userDao The {@link UserDAO Product Data Access Object} to perform CRUD operations
+     * @param userDao The {@link UserDAO User Data Access Object} to perform CRUD operations
      * <br>
      * This dependency is injected by the Spring Framework
      */
@@ -64,7 +64,7 @@ public class UserController {
     }
 
     /**
-     * Creates a {@linkplain User user} with the provided item object
+     * Creates a {@linkplain User user} with the provided userg object
      * 
      * @param user - The {@link User user} to create
      * 
