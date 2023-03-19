2e4a3d97156ba23b0d2022f81761abfae405647f
zrb8768 <zrb8768@rit.edu>
2023-03-18 23:40:15 -0400

    missed file from last commit

---
+++ estore-ui/src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../user';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.userService.getUsers()
+    .subscribe(users => this.users = users);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.userService.addUser({ name } as unknown as User)
+      .subscribe(user => {
+        this.users.push(user);
+      });
+  }
+
+}
