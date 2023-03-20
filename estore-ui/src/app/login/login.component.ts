801945b151e28e598766680355a0a5b0cb99e939
Jacob Karvelis <jak9527@rit.edu>
2023-03-19 22:39:17 -0400

    Finished the cart services. Not 100% sure most of the methods work, but adding a cart does. Modified login service to create a new cart each time a new user is created, and they have matching ids

--- estore-ui/src/app/login/login.component.ts
+++ estore-ui/src/app/login/login.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import { UserService } from '../user.service';
 import { CurrentUserService } from '../currentUser.service';
+import { Cart } from '../cart';
+import { CartService } from '../cart.service';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-login',
@@ -11,9 +14,11 @@ import { CurrentUserService } from '../currentUser.service';
 })
 export class LoginComponent implements OnInit {
   user: User = null as unknown as User;
+  cart: Cart = null as unknown as Cart;
 
   constructor(private userService: UserService,
-    private currentUserService: CurrentUserService) { }
+    private currentUserService: CurrentUserService,
+    private cartService: CartService) { }
 
   ngOnInit(): void {
   }
@@ -29,13 +34,22 @@ export class LoginComponent implements OnInit {
     this.userService.getUser(name)
     .subscribe(newuser => { 
         this.user = newuser
-        if (this.user === null){
+        if (this.user == null){
             this.userService.addUser({"id":0,"username":name} as User)
               .subscribe(newuser => {
                 this.user = newuser;
+                this.cartService.addCart({"id":0, "products": new Map<number, Product>()} as Cart)
+                    .subscribe(newCart => {
+                    this.cart = newCart;
+                });
+                this.currentUserService.setCurrentUser(this.user).subscribe();
               });
+            
         }
-        this.currentUserService.setCurrentUser(this.user).subscribe();
+        else{
+            this.currentUserService.setCurrentUser(this.user).subscribe();
+        }
+        
     });
     
     
