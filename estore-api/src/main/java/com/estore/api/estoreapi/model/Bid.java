88b5a3b303bc544628dab5787c1f7876030d1daf
Jacob Karvelis <jak9527@rit.edu>
2023-03-26 16:34:08 -0400

    added documentation to the Bid class

--- estore-api/src/main/java/com/estore/api/estoreapi/model/Bid.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Bid.java
@@ -1,18 +1,36 @@
 package com.estore.api.estoreapi.model;
 
+/**
+ * Represents an Bid
+ * 
+ * @author Jacob Karvelis jak9527
+ */
 public class Bid {
     private float bid;
     private User user;
 
+    /**
+     * Creates a bid with the give bid price and User who made it
+     * @param bid Price of bid
+     * @param user User who made the bid
+     */
     public Bid(float bid, User user){
         this.bid = bid;
         this.user = user;
     }
 
+    /**
+     * Get the price of this bid
+     * @return bid price
+     */
     public float getBidPrice(){
         return bid;
     }
 
+    /**
+     * get the name of the user associated with this bid
+     * @return the username of the user associated with this bid
+     */
     public String getUserName(){
         return user.getUsername();
     }
