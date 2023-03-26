171eb6b1a1a4937f9d8235140116c9645a6694f1
Jacob Karvelis <jak9527@rit.edu>
2023-03-26 16:41:17 -0400

    I believe the DAO now has all methods that will be needed. If more are necessary later, I will add them

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/AuctionItemDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/AuctionItemDAO.java
@@ -2,10 +2,14 @@ package com.estore.api.estoreapi.persistence;
 
 import java.time.LocalDateTime;
 
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Io;
+
 import java.io.IOException;
 
 import com.estore.api.estoreapi.model.Product;
+import com.estore.api.estoreapi.model.User;
 import com.estore.api.estoreapi.model.AuctionItem;
+import com.estore.api.estoreapi.model.Bid;
 
 /**
  * Defines the interface for Auction Item object persistence
@@ -35,6 +39,17 @@ public interface AuctionItemDAO {
      * @throws IOException if an issue with underlying storage
      */
     AuctionItem getAuction() throws IOException;
+
+    /**
+     * Retrieves the {@linkplain AuctionItem auction} with the given aId
+     * 
+     * @return a {@link AuctionItem auction} with the given aId
+     * <br>
+     * null if no {@link AuctionItem auction} exists with that aId
+     * 
+     * @throws IOException if an issue with underlying storage
+     */
+    AuctionItem getAuction(int aId) throws IOException;
 
     /**
      * Creates and saves a {@linkplain AuctionItem auction}
@@ -76,4 +91,15 @@ public interface AuctionItemDAO {
      */
     boolean deleteAuction(int id) throws IOException;
 
+    /**
+     * Place a bid on the auction with the given aId
+     * 
+     * @param user The user that placed this bid
+     * @param bid The price they wanted to bid
+     * @return true if the bid was placed
+     * <br>
+     * false if the bid was not placed due to being lower than the max bid
+     * @throws IOException if underlying storage cannot be accessed
+     */
+    boolean placeBid(User user, float bid) throws IOException;
 }
